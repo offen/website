465d8707b0e0a36046e4ebb74bf7e463f3be49d5
Hendrik Niefeld <hello@niefeld.com>
2020-04-28 15:51:34 +0200

    text updates, minor layout tweaks

--- homepage/pelicanconf.py
+++ homepage/pelicanconf.py
@@ -77,6 +77,6 @@ TWITTER_URL = 'https://twitter.com/hioffen'
 RELEASE_DIRECT_URL = 'https://get.offen.dev'
 DOCKER_DIRECT_URL = 'https://hub.docker.com/r/offen/offen'
 HEROKU_DIRECT_URL = 'https://heroku.com/deploy?template=https://github.com/offen/heroku/tree/master'
-OFFEN_AUDITORIUM_URL = 'https://analytics.offen.dev/'
+OFFEN_AUDITORIUM_URL = 'https://analytics.offen.dev/auditorium'
 
 OFFEN_ACCOUNT_ID = os.environ.get('OFFEN_ACCOUNT_ID', None)
